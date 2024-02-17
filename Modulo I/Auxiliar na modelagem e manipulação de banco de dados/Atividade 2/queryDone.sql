dc2d3dd449aed086ce718bced1dd71e675213e9e
Emerson S <eyenessus@gmail.com>
2024-02-17 11:54:47 -0300

    Correções

---
+++ Modulo I/Auxiliar na modelagem e manipulação de banco de dados/Atividade 2/queryDone.sql
@@ -0,0 +1,93 @@
+    CREATE DATABASE hospital_db_enoun;
+    USE hospital_db_enoun;
+
+
+    CREATE TABLE convenios (
+        id_convenios int AUTO_INCREMENT PRIMARY KEY,
+        nome varchar(255),
+        cnpj varchar(255),
+        tempo_carencia DATE,
+        paciente_id int
+    );
+
+CREATE TABLE pacientes(
+    id_pacientes int AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(255),
+    data_nascimento date,
+    cpf varchar(11),
+    rg varchar(11),
+    email varchar(255),
+    convenio_id int default NULL,
+    FOREIGN KEY (convenio_id) REFERENCES convenios(id_convenios)
+);
+    CREATE TABLE especialidades_medicos(
+        id_especialidades_medicos int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        especialidade varchar(45)
+    );
+
+    CREATE TABLE medicos(
+        idmedicos INT PRIMARY KEY AUTO_INCREMENT,
+        nome varchar(255),
+        data_nascimento DATE,
+        cpf varchar(11),
+        crm int,
+        uc_crm CHAR(2),
+        especialidade_id int,
+        telefone varchar(45),
+        endereco_completo varchar(45),
+        FOREIGN KEY (especialidade_id) REFERENCES especialidades_medicos(id_especialidades_medicos)
+    );
+
+    CREATE TABLE receitas(
+        id_receitas int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        receita text
+    );
+
+    CREATE TABLE consultas (
+        idconsultas int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        data_hora DATE,
+        paciente_id INT,
+        valor decimal(10,2),
+        especialidades_medico_id int,
+        receitas int,
+        fk_medico_responsavel INT ,
+        FOREIGN KEY (fk_medico_responsavel) REFERENCES medicos(idmedicos),
+        FOREIGN KEY (paciente_id) REFERENCES pacientes(id_pacientes),
+        FOREIGN KEY (especialidades_medico_id) REFERENCES especialidades_medicos(id_especialidades_medicos),
+        FOREIGN KEY (receitas) REFERENCES receitas(id_receitas)
+    );
+
+
+    CREATE TABLE tipo_quarto(
+        id_tipo_quarto int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        descricao varchar(255),
+        valor_diaria decimal(10,2)
+    );
+
+    CREATE TABLE quartos(
+        id_quartos int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        numero int,
+        id_tipo_quarto int ,
+        FOREIGN KEY (id_tipo_quarto) REFERENCES tipo_quarto(id_tipo_quarto)
+    );
+
+    CREATE TABLE enfermeiros(
+        id_enfermeiros int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        nome varchar(255),
+        cpf varchar(11),
+        cre int
+        );
+        
+    CREATE TABLE internacoes (
+        id_internacoes int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        data_entrada DATE,
+        data_prev_alta DATE,
+        procedimento text,
+        paciente_id INT,
+        quarto_id INT,
+        enfermeiro_id INT,
+        FOREIGN KEY (quarto_id) REFERENCES quartos(id_quartos),
+        FOREIGN KEY (enfermeiro_id) REFERENCES enfermeiros(id_enfermeiros),
+        FOREIGN KEY (paciente_id) REFERENCES pacientes(id_pacientes)
+    );
+
---
+++ Modulo I/Auxiliar na modelagem e manipulação de banco de dados/Atividade 3/query.sql
@@ -0,0 +1,82 @@
+
+INSERT INTO receitas(receita) VALUES ("Dipirona 1 comprimido de 6 em 6 horas por 3 dias");
+INSERT INTO receitas(receita) VALUES ("Paracetamol 1 comprimido de 6 em 6 horas por 3 dias");
+INSERT INTO receitas(receita) VALUES ("Ibuprofeno 1 comprimido de 6 em 6 horas por 3 dias");
+INSERT INTO receitas(receita) VALUES ("Dipirona 1 comprimido de 6 em 6 horas por 3 dias");
+
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("Maria","123",1);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("João","123",2);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("José","123",3);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("Ana","123",4);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("Paulo","123",5);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("Pedro","123",6);
+INSERT INTO enfermeiros(nome,cpf,cre) VALUES ("Fernanda","123",7);
+    
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Pediatra');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Clínica Geral');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Gastroenterologia');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Dermatologista');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Ginecologista');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Urologista');
+    INSERT INTO especialidades_medicos(especialidade) VALUES ('Neurologista');
+
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Ana","1980-01-01","12345678901",123456,"SP",5,"(11) 1234-5678","Rua 1, 123");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Paulo","1980-01-01","12345678901",123456,"SP",6,"(11) 1234-5678","Rua 1, 123");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Pedro","1980-01-01","12345678901",123456,"SP",7,"(11) 1234-5678","Rua 1, 123");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Fernanda","1985-05-10","98765432109",654321,"RJ",1,"(21) 9876-5432","Avenida 2, 456");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Carlos","1978-09-15","56789012345",789012,"MG",2,"(31) 5678-9012","Rua 3, 789");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Juliana","1990-03-25","34567890123",890123,"RS",3,"(51) 3456-7890","Avenida 4, 012");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Ricardo","1982-07-12","67890123456",901234,"SC",4,"(48) 6789-0123","Rua 5, 234");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Camila","1987-11-30","45678901234",123456,"SP",5,"(11) 4567-8901","Avenida 6, 567");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Lucas","1995-02-18","89012345678",345678,"PR",6,"(41) 8901-2345","Rua 7, 890");
+    INSERT INTO medicos(nome,data_nascimento,cpf,crm,uc_crm,especialidade_id,telefone,endereco_completo) VALUES ("Mariana","1989-06-05","23456789012",567890,"BA",7,"(71) 2345-6789","Avenida 8, 901");
+    
+ 
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Maria","1980-01-01","11111111111","123456789","teste@gmail.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("João","1980-01-01","22222222222","123456789","tt@gmail.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("José","1980-01-01","33333333333","123456789","amz@amz.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Ana","1985-05-10","44444444444","987654321","xx@apple.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Paulo","1978-09-15","55555555555","567890123","pop@apple.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Pedro","1990-03-25","66666666666","345678901","ss@apple.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Fernanda","1982-07-12","77777777777","678901234","xx@te.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Carlos","1987-11-30","88888888888","456789012","hoj@gmail.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Juliana","1995-02-18","99999999999","890123456","xx@gmail.com");
+INSERT INTO pacientes(nome,data_nascimento,cpf,rg,email) VALUES ("Ricardo","1989-06-05","00000000000","234567890","goku@gmail.com");
+
+
+
+    INSERT INTO convenios(nome,cnpj,tempo_carencia) VALUES ("Unimed","12345678901234","2021-01-01");
+    INSERT INTO convenios(nome,cnpj,tempo_carencia) VALUES ("Amil","12345678901234","2021-01-01");
+    INSERT INTO convenios(nome,cnpj,tempo_carencia) VALUES ("Sulamerica","12345678901234","2021-01-01");
+    INSERT INTO convenios(nome,cnpj,tempo_carencia) VALUES ("Bradesco","12345678901234","2021-01-01");
+ 
+
+    INSERT INTO consultas(data_hora,paciente_id,valor,especialidades_medico_id,receitas,fk_medico_responsavel) VALUES ("2019-05-15",1,223.00,1,1,1);
+    INSERT INTO consultas(data_hora,paciente_id,valor,especialidades_medico_id,receitas,fk_medico_responsavel) VALUES ("2018-07-20",1,323.00,2,2,2);
+    INSERT INTO consultas(data_hora,paciente_id,valor,especialidades_medico_id,receitas,fk_medico_responsavel) VALUES ("2020-10-10",1,533.00,3,3,3);
+    INSERT INTO consultas(data_hora,paciente_id,valor,especialidades_medico_id,receitas,fk_medico_responsavel) VALUES ("2017-12-05",4,221.00,4,2,4);
+    INSERT INTO consultas(data_hora,paciente_id,valor,especialidades_medico_id,receitas,fk_medico_responsavel) VALUES ("2022-03-25",2,4345.00,5,2,5);
+  
+    
+
+    INSERT INTO tipo_quarto(descricao,valor_diaria) VALUES ("apartamentos",100.00);
+    INSERT INTO tipo_quarto(descricao,valor_diaria) VALUES ("quartos duplos ",200.00);
+    INSERT INTO tipo_quarto(descricao,valor_diaria) VALUES ("enfermaria",300.00);
+
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (1,1);
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (2,1);
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (3,2);
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (4,2);
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (5,3);
+INSERT INTO quartos(numero,id_tipo_quarto) VALUES (6,3);
+
+
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",2,1,1);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",2,2,2);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",3,3,3);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",4,3,4);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",5,3,5);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",5,2,6);
+INSERT INTO internacoes(data_entrada,data_prev_alta,procedimento,paciente_id,quarto_id,enfermeiro_id) VALUES ("2021-01-01","2021-01-10","Cirurgia de apendicite",7,1,2);
+
+
