84e02cd77b03a186951b884b6e8d6bea2c1fe058
E.S <eyenessus@gmail.com>
2024-03-29 17:05:44 -0300

    [REFATOR] Otimização da terceira consulta de 3minutos para 80 segundos

--- Modulo I/Auxiliar na administração de banco de dados/Atividade 3/performance.sql
+++ Modulo I/Auxiliar na administração de banco de dados/Atividade 3/performance.sql
@@ -1,11 +1,11 @@
 /*
-consulta para um relatório de todas as vendas pagas em dinheiro. 
-Necessários para o relatório data da venda, valor total; produtos vendidos, quantidade e valor unitário; nome do cliente, cpf e telefone.
-Ordena-se pela data de venda, as mais recentes primeiro.
-**/
-
+ consulta para um relatório de todas as vendas pagas em dinheiro. 
+ Necessários para o relatório data da venda, valor total; produtos vendidos, quantidade e valor unitário; nome do cliente, cpf e telefone.
+ Ordena-se pela data de venda, as mais recentes primeiro.
+ **/
 CREATE INDEX idx_pay ON venda(id);
-CREATE VIEW relatorio_vendas AS SELECT
+CREATE VIEW relatorio_vendas AS
+SELECT
     V.data,
     V.valor_total,
     P.nome,
@@ -25,13 +25,47 @@ WHERE
 ORDER BY
     V.data;
 
+/***
+ consulta para encontrar todas as vendas de produtos de um dado fabricante
+ Mostrar dados do produto, quantidade vendida, data da venda.
+ Ordena-se pelo nome do produto.
+ ***/
+CREATE INDEX indx_fabricant ON produto(fabricante);
+CREATE VIEW vendas_fabricante AS
+SELECT
+    p.nome,
+    iv.quantidade,
+    v.data
+FROM
+    produto as p
+    JOIN item_venda as iv ON p.id = iv.produto_id
+    JOIN venda as v ON v.id = iv.venda_id
+WHERE
+    p.fabricante like '%lar%';
 
 /***
-consulta para encontrar todas as vendas de produtos de um dado fabricante
-Mostrar dados do produto, quantidade vendida, data da venda.
-Ordena-se pelo nome do produto.
-***/
-CREATE INDEX indx_fabricant ON produto(fabricante);
-CREATE VIEW vendas_fabricante AS SELECT p.nome, iv.quantidade, v.data
-FROM produto as p JOIN item_venda as iv ON p.id = iv.produto_id JOIN venda as v ON v.id = iv.venda_id
-WHERE p.fabricante like '%lar%';
+ Relatório de vendas de produto por cliente.
+ Mostrar dados do cliente, dados do produto e valor e quantidade totais de venda ao cliente de cada produto.
+ */
+CREATE INDEX indx_sub ON item_venda(subtotal);
+CREATE INDEX index_quant ON item_venda(quantidade);
+CREATE INDEX indx_pnome ON produto(nome);
+CREATE INDEX indx_cnome ON cliente(nome);
+CREATE VIEW relatorio_produtos_cliente AS
+SELECT
+    c.nome,
+    c.cpf,
+    c.endereco,
+    c.telefone,
+    p.nome as nomeProduto,
+    p.descricao,
+    iv.quantidade,
+    IV.valor_unitario
+FROM
+    produto AS p
+    JOIN item_venda AS iv ON p.id = iv.produto_id
+    JOIN venda AS v ON iv.venda_id = v.id
+    JOIN cliente AS c ON c.id = v.cliente_id
+ORDER BY
+    c.nome,
+    p.nome;
